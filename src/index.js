ee3d9d4d34da836037cb1df5935266e214634441
eramaan <8112194+eramaan@users.noreply.github.com>
2024-10-01 16:12:26 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+
+// remember, if you need to use an image in a js file, you need to import it:
+// import odinImage from "./odin.png";
+// const image = document.createElement("img");
+// image.src = odinImage;
